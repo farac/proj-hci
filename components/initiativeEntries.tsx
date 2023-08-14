10c5b4e3fdb4d6630b3a0228ee66b5ba1a3bac7e
farac <brunofarac@gmail.com>
2023-08-14 16:17:59 +0200

    yarn, firebase

--- components/initiativeEntries.tsx
+++ components/initiativeEntries.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import e from "../constants/entries.json";
 import Icons from "../public/conditions/importCondIcons";
-import { ReactNode } from "react";
 
 import styles from "@/components/initiativeEntries.module.scss";
 
-import IconBelt from "./iconBelt";
+import IconBelt from "./initiative_entry_components/iconBelt";
+import HealthHolder from "./initiative_entry_components/healthHolder";
 
 export default function IntiativeEntries() {
   const listEntries = e.map((entry) => (
@@ -19,18 +19,10 @@ export default function IntiativeEntries() {
       <div className={styles.rightsideHolder}>
         <p className={styles.nameText}>{entry.name}</p>
         <div className={styles.bottomRowHolder}>
-          <div className={styles.healthHolder}>
-            <p
-              className={
-                entry.current_hp / entry.max_hp > 0.5
-                  ? styles.hpCurrentDefault
-                  : styles.hpCurrentBloodied
-              }
-            >
-              {entry.current_hp}
-            </p>
-            <p className={styles.hpMax}>/{entry.max_hp}</p>
-          </div>
+          <HealthHolder
+            currentHp={entry.current_hp}
+            maxHp={entry.max_hp}
+          ></HealthHolder>
           <IconBelt conditions={entry.conditions}></IconBelt>
         </div>
       </div>
