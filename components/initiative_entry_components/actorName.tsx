31d136af094f3b0a3f32b70dfa123b71e3c6d558
farac <brunofarac@gmail.com>
2023-08-15 18:44:49 +0200

    name editing hooked to db

--- components/initiative_entry_components/actorName.tsx
+++ components/initiative_entry_components/actorName.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, update } from "firebase/database";
 import { database } from "@/firebaseConfig";
 import * as Popover from "@radix-ui/react-popover";
 
@@ -15,6 +15,7 @@ export default function ActorName({
   entryId: number;
 }) {
   const [name, setName] = useState<string>("");
+  const [nameEditing, setNameEdited] = useState<string>("");
 
   useEffect(() => {
     const nameRef = ref(
@@ -27,6 +28,18 @@ export default function ActorName({
     });
   });
 
+  function onEditName(newName: string) {
+    if (newName && name != newName) {
+      console.log(newName);
+      const entryRef = ref(
+        database,
+        "sessions/" + sessionId + "/entries/" + entryId
+      );
+      update(entryRef, { name: newName });
+      setNameEdited("");
+    }
+  }
+
   return (
     <div className={styles.holder}>
       <Popover.Root>
@@ -34,12 +47,21 @@ export default function ActorName({
           <p className={styles.nameText}>{name}</p>
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className={styles.PopoverContent} sideOffset={5}>
+          <Popover.Content
+            className={styles.PopoverContent}
+            sideOffset={0}
+            onCloseAutoFocus={() => onEditName(nameEditing)}
+          >
             <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
               <p className={styles.Text} style={{ marginBottom: 10 }}>
                 Change entry name:
               </p>
-              <input className={styles.Input} id="width" defaultValue={name} />
+              <input
+                className={styles.Input}
+                id="width"
+                defaultValue={name}
+                onChange={(e) => setNameEdited(e.target.value)}
+              />
             </div>
             <Popover.Close className={styles.PopoverClose} aria-label="Close">
               <Image
