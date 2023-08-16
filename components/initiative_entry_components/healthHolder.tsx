d8fe01384370ece62ff1e3c62ea18662ab0853f4
farac <brunofarac@gmail.com>
2023-08-16 19:57:58 +0200

    init, hp firebase

--- components/initiative_entry_components/healthHolder.tsx
+++ components/initiative_entry_components/healthHolder.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
-
-import styles from "@/components/initiative_entry_components/healthHolder.module.scss";
+import { ref, onValue, update } from "firebase/database";
 import { database } from "@/firebaseConfig";
+import * as Popover from "@radix-ui/react-popover";
+
+import * as ExitIcon from "@/public/cancel.svg";
+import styles from "@/components/initiative_entry_components/healthHolder.module.scss";
 
 export default function HealthHolder({
   sessionId,
@@ -13,6 +16,9 @@ export default function HealthHolder({
 }) {
   const [currentHp, setCurrentHp] = useState<number>(0);
   const [maxHp, setMaxHp] = useState<number>(1);
+
+  const [currentHpEditing, setCurrentHpEditing] = useState<string>("");
+  const [maxHpEditing, setMaxHpEditing] = useState<string>("");
 
   useEffect(() => {
     const currentHpRef = ref(
@@ -33,18 +39,80 @@ export default function HealthHolder({
     });
   });
 
+  function onEditHp(newCurrentHp: string, newMaxHp: string) {
+    if (isNaN(Number(newCurrentHp)) || isNaN(Number(newMaxHp))) {
+      // todo
+      console.log("check inputs");
+      return;
+    }
+    const entryRef = ref(
+      database,
+      "sessions/" + sessionId + "/entries/" + entryId
+    );
+
+    if (newCurrentHp && currentHp != Number(newCurrentHp)) {
+      update(entryRef, { current_hp: newCurrentHp });
+      setCurrentHpEditing("");
+    }
+
+    if (newMaxHp && maxHp != Number(newMaxHp)) {
+      update(entryRef, { max_hp: newMaxHp });
+      setMaxHpEditing("");
+    }
+  }
+
   return (
-    <div className={styles.healthHolder}>
-      <p
-        className={
-          currentHp / maxHp > 0.5
-            ? styles.hpCurrentDefault
-            : styles.hpCurrentBloodied
-        }
-      >
-        {currentHp}
-      </p>
-      <p className={styles.hpMax}>/{maxHp}</p>
-    </div>
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <div className={styles.healthHolder}>
+          <p
+            className={
+              currentHp / maxHp > 1
+                ? styles.hpCurrentTempHp
+                : currentHp / maxHp < 0.5
+                ? styles.hpCurrentBloodied
+                : styles.hpCurrentDefault
+            }
+          >
+            {currentHp}
+          </p>
+          <p className={styles.hpMax}>/{maxHp}</p>
+        </div>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content
+          className={styles.PopoverContent}
+          sideOffset={0}
+          onCloseAutoFocus={() => onEditHp(currentHpEditing, maxHpEditing)}
+        >
+          <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+            <p className={styles.Text} style={{ marginBottom: 10 }}>
+              Change health:
+            </p>
+            <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+              <input
+                className={styles.Input}
+                id="currentHp"
+                defaultValue={currentHp}
+                onChange={(e) => setCurrentHpEditing(e.target.value)}
+              />
+              <p className={styles.TextSlash} style={{ marginBottom: 10 }}>
+                /
+              </p>
+              <input
+                className={styles.Input}
+                id="maxHp"
+                defaultValue={maxHp}
+                onChange={(e) => setMaxHpEditing(e.target.value)}
+              />
+            </div>
+          </div>
+          <Popover.Close className={styles.PopoverClose} aria-label="Close">
+            <Image className={styles.IconButton} src={ExitIcon} alt=""></Image>
+          </Popover.Close>
+          <Popover.Arrow className={styles.PopoverArrow} />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
--- components/initiative_entry_components/initiativeBlock.tsx
+++ components/initiative_entry_components/initiativeBlock.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, update } from "firebase/database";
 import { database } from "@/firebaseConfig";
+import * as Popover from "@radix-ui/react-popover";
 
+import * as ExitIcon from "@/public/cancel.svg";
 import styles from "@/components/initiative_entry_components/initiativeBlock.module.scss";
 
 export default function InitiativeBlock({
@@ -12,6 +15,8 @@ export default function InitiativeBlock({
   entryId: number;
 }) {
   const [initiativeRoll, setInitiativeRoll] = useState<number>(20);
+  const [initiativeRollEditing, setInitiativeRollEditing] =
+    useState<string>("");
 
   useEffect(() => {
     const initiativeRollRef = ref(
@@ -24,5 +29,58 @@ export default function InitiativeBlock({
     });
   });
 
-  return <p className={styles.initiativeBlock}>{initiativeRoll}</p>;
+  function onEditInitiativeRoll(newRoll: string) {
+    if (isNaN(Number(newRoll))) {
+      // todo
+      console.log("check inputs");
+      return;
+    }
+
+    if (newRoll && initiativeRoll != Number(newRoll)) {
+      const entryRef = ref(
+        database,
+        "sessions/" + sessionId + "/entries/" + entryId
+      );
+      update(entryRef, { initiative_roll: newRoll });
+      setInitiativeRollEditing("");
+    }
+  }
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <p className={styles.initiativeBlock}>{initiativeRoll}</p>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content
+          className={styles.PopoverContent}
+          sideOffset={0}
+          onCloseAutoFocus={() => onEditInitiativeRoll(initiativeRollEditing)}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 10,
+            }}
+          >
+            <p className={styles.Text} style={{ marginBottom: 10 }}>
+              Change initiative:
+            </p>
+            <input
+              type="number"
+              className={styles.Input}
+              id="initiativeRoll"
+              defaultValue={initiativeRoll}
+              onChange={(e) => setInitiativeRollEditing(e.target.value)}
+            />
+          </div>
+          <Popover.Close className={styles.PopoverClose} aria-label="Close">
+            <Image className={styles.IconButton} src={ExitIcon} alt=""></Image>
+          </Popover.Close>
+          <Popover.Arrow className={styles.PopoverArrow} />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
 }
